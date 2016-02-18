dc0be83821ee7198df44fb82e78e08cb86041d27
Eggceptions <eggceptions@gmail.com>
2016-02-18 14:29:43 +0100

    Handling requests and saving history

---
+++ src/Request.java
@@ -0,0 +1,29 @@
+public class Request {
+    private long time;
+    private String ip;
+    private String action;
+    private String argument;
+
+    public Request(String ip, String action, String argument) {
+        this.time = System.currentTimeMillis();
+        this.ip = ip;
+        this.action = action;
+        this.argument = argument;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public String getArgument() {
+        return argument;
+    }
+}
--- src/Server.java
+++ src/Server.java
@@ -4,26 +4,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.TimeZone;
 
-/**
- * Created by Ferdinand on 17.02.2016.
- */
 public class Server {
 
     public static void main(String[] args) {
-
+        ArrayList<Request> history = new ArrayList<>();
+        int number = 9001;
         String melding = "";
         String nyMelding = "Hei hei";
         ServerSocket serverSocket = null;
-
-
         try {
             serverSocket = new ServerSocket(7001);
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(0);
         }
-
         while (true) {
             System.out.println("Server is running...");
             try {
@@ -32,22 +31,67 @@ public class Server {
                 DataOutputStream out = new DataOutputStream(connectionSocket.getOutputStream());
 
                 melding = in.readLine();
-
-                System.out.println("Melding fra klient: " + melding);
-
-//                out.writeBytes(nyMelding + "\n");
+                String ip = connectionSocket.getInetAddress().getHostName();
+                System.out.println("New request from " + ip + ": " + melding);
+                String[] words = melding.split(" ");
+                if (words.length == 3) {
+                    if (words[2].equals("GET")) {
+                        history.add(new Request(ip, "GET", "NULL"));
+                        out.writeBytes("NUMBER " + number + "\n");
+                    } else if (words[2].equals("HISTORY")) {
+                        String historyMessage = "";
+                        for (Request r : history) {
+                            Date date = new Date(r.getTime());
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+                            sdf.setTimeZone(TimeZone.getTimeZone("GMT+1"));
+                            String formattedDate = sdf.format(date);
+                            historyMessage += formattedDate + " : " + r.getIp() + " : " + r.getAction() + " : " + r.getArgument() + "\n";
+                        }
+                        out.writeBytes(historyMessage + "\n");
+                    }
+                } else if (words.length == 4) {
+                    int newNumber = -1;
+                    switch (words[2]) {
+                        case "ADD":
+                            newNumber = -1;
+                            try {
+                                newNumber = Integer.parseInt(words[3]);
+                            } catch (NumberFormatException e) {
+                                out.writeBytes("ERROR: Request didn't succeed. Invalid format.\n");
+                            }
+                            if (newNumber != -1) {
+                                number += newNumber;
+                                history.add(new Request(ip, "ADD", "" + newNumber));
+                                out.writeBytes("OK\n");
+                            }
+                            break;
+                        case "SUB":
+                            newNumber = -1;
+                            try {
+                                newNumber = Integer.parseInt(words[3]);
+                            } catch (NumberFormatException e) {
+                                out.writeBytes("ERROR: Request didn't succeed. The number to add must be a number.\n");
+                            }
+                            if (newNumber != -1) {
+                                number -= newNumber;
+                                history.add(new Request(ip, "SUB", "" + newNumber));
+                                out.writeBytes("OK\n");
+                            }
+                            break;
+                        default:
+                            out.writeBytes("ERROR: Request didn't succeed. Invalid argument.\n");
+                            break;
+                    }
+                }
             } catch (Exception e) {
                 e.printStackTrace();
                 break;
             }
         }
-
         try {
             serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
 }
